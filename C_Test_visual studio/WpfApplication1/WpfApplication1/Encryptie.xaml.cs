4ec4badbdd8fbc0143be6864f4a81a60584f8bd3
Yannick Lemmens <lemmens.9@gmail.com>
2015-04-01 11:40:30 +0200

    Add alles

---
+++ C_Test_visual studio/WpfApplication1/WpfApplication1/Encryptie.xaml.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for Encryptie.xaml
+    /// </summary>
+    public partial class Encryptie : Window
+    {
+        
+        public Encryptie()
+        {
+            InitializeComponent();
+            encryptieTextbox.IsReadOnly = true;
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new System.Windows.Forms.OpenFileDialog();
+            fileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            var result = fileDialog.ShowDialog();
+            switch (result)
+            {
+                case System.Windows.Forms.DialogResult.OK:
+                    var file = fileDialog.FileName;
+                    StreamReader sr = new StreamReader(file);
+                    encryptieTextbox.Text = sr.ReadToEnd();
+                    break;
+                case System.Windows.Forms.DialogResult.Cancel:
+                    encryptieTextbox.Text = "Kies een bestand en klik op OK";
+                    break;
+            }
+
+        }
+
+        private void Start_Encryptie(object sender, RoutedEventArgs e)
+        {
+            String encString = encryptieTextbox.Text;
+            this.Close();
+            EncryptedPage encPage = new EncryptedPage();
+            encPage.Show();
+            using (Aes myAes = Aes.Create())
+            {
+
+                 //Encrypt the string to an array of bytes. 
+                byte[] encrypted = EncryptStringToBytes_Aes(encString,myAes.Key, myAes.IV);
+                string encKey = System.Text.Encoding.UTF8.GetString(encrypted);
+                encPage.encryptedlabel.Content = encKey;
+
+                
+                System.IO.File.WriteAllText(@".\aeskey.txt", encKey);
+            }
+
+                
+        
+
+        }
+
+
+
+        static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key,byte[] IV)
+        {
+            // Check arguments. 
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("Key");
+            byte[] encrypted;
+            // Create an Aes object 
+            // with the specified key and IV. 
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption. 
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                            
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+
+            // Return the encrypted bytes from the memory stream. 
+            return encrypted;
+
+        }
+
+        static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            // Check arguments. 
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("Key");
+
+            // Declare the string used to hold 
+            // the decrypted text. 
+            string plaintext = null;
+
+            // Create an Aes object 
+            // with the specified key and IV. 
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption. 
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                                // and place them in a string.
+                                                        plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+
+            return plaintext;
+
+        }
+
+        public static void GenerateRSAKeyPair(out string publicKey, out string privateKey)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(2048);
+            publicKey = rsa.ToXmlString(false);
+            privateKey = rsa.ToXmlString(true);
+        }
+        
+
+    }     
+}
---
+++ C_Test_visual studio/WpfApplication1/WpfApplication1/mainwindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Encrypt(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Encryptie encScherm = new Encryptie();
+            encScherm.Show();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("test");
+        }
+
+        private void Decrypt(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("decryptie");
+        }
+
+       
+    }
+}
